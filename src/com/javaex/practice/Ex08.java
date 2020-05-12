9988c6206aa873375de05f3d75a3a16fa47f024a
hi <hi@DESKTOP-53P5LLN>
2020-05-12 15:32:14 +0900

    배열을 출력하는 for문을 따로 만듦.

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -11,6 +11,7 @@ public class Ex08 {
 		int lotto[] = new int[6];
 				
 		for(int i = 0; i < lotto.length; i++) {
+			
 			int lottoNum = (int)(Math.random() * 45) + 1;
 			lotto[i]= lottoNum;
 			
@@ -19,10 +20,12 @@ public class Ex08 {
 					i--;
 				}
 			}
-			
-			System.out.print(lotto[i] + "    ");
 		}
 		
+		
+		for(int i = 0; i <lotto.length; i++) {
+			System.out.print(lotto[i] + "    ");
+		}
 
 	}
 }
